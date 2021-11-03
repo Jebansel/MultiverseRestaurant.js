d04d6d756600a5359cfdbc20072fce14a2b19676
Jeban <jebansel@gmail.com>
2021-11-03 09:50:34 +0000

    Added through postman more images to the restaurants.

---
+++ SequelizeFrontEnd/routes/restaurants.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Router = express.Router();
+const fetch = require('node-fetch');
+const url = "http://localhost:3002/api/restaurants"; // http://localhost:3002/api/restaurants
+
+Router
+  // READ
+  .get('/', async (req, res, next) => {
+    try {
+      const response = await fetch(url);
+      const restaurants = await response.json();
+      res.render('restaurants', { restaurants });
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+module.exports = Router;
